4090b7031931f791b7f3694c4370000460c9c3bd
Sabarish <avsabarish@gmail.com>
2024-04-05 07:04:57 +0530

    responsive

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios"
 import { useNavigate } from 'react-router-dom';
 import MessageTemplate from "./MessageTemplate";
+import "../index.css"
 
 function Signup(){
 
@@ -50,17 +51,17 @@ function Signup(){
     {isSignedUp?
     <MessageTemplate message="User created successfully"/>
     :
-    <div className="flex justify-evenly items-center h-screen">
+    <div id="container-div" className="flex justify-evenly items-center h-screen">
 
-<div className="h-2/3 w-1/3 rounded-lg flex flex-col items-center text-white pt-32">
+<div id="text-container" className="h-2/3 w-1/3 rounded-lg flex flex-col items-center text-white pt-32">
 <h1 className="text-purple-500 font-mono text-7xl font-black">My Blog</h1>
     <p className="pt-10 text-xl text-purple-500">Blog like a 
     <span className="font-extrabold text-black pl-1">BOSS</span></p>
 </div>
 
-<div className="bg-purple-500 h-2/3 w-1/3 rounded-lg flex flex-col items-center text-white">
+<div id="form-container" className="bg-purple-500 h-2/3 w-1/3 rounded-lg flex flex-col items-center text-white">
     <h1 className="pt-5 text-6xl font-bold">Sign Up</h1>
-    <div className="p-10">
+    <div id="form-inner-container" className="p-10">
     <div className="mb-9 relative">
     <label htmlFor="" className="font-bold">Username:</label> <br />
     <input type="text" id="username" name="username" className="font-semibold w-96 focus:outline-none text-black rounded-sm pl-2" onChange={()=>{setUsernameError("")}}/> <br />
